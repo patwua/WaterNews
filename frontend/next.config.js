2463fc86dd4d2b16d3d672930f6ac0c958fec3a3
patwua <patwuablog@gmail.com>
2025-08-26 10:27:50 -0400

    chore: convert next config to JavaScript

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,5 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: false,
   images: {
     // Modern config (replaces deprecated images.domains)
@@ -52,4 +51,4 @@ const nextConfig: NextConfig = {
   // },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
