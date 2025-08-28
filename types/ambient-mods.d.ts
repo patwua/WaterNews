a16392334ac541c0b7b5539c2854aee339942af6
patwua <patwuablog@gmail.com>
2025-08-28 10:08:46 -0400

    chore: remove marked dependency

--- types/ambient-mods.d.ts
+++ types/ambient-mods.d.ts
@@ -1,5 +1,4 @@
 // Loosen unresolved packages when registry blocks installs
-declare module "marked" { const marked: any; export { marked }; export default marked; }
 declare module "prop-types" { const v: any; export default v; export = v; }
 declare module "webidl-conversions" { const v: any; export default v; export = v; }
 declare module "whatwg-url" { const v: any; export default v; export = v; }
